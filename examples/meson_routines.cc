121a8489278dbf21c990479d7d63caadb8d890cf
Craig McNeile <craig.mcneile@plymouth.ac.uk>
2019-06-17 16:51:23 +0100

    The code now agrees with the MILC code.

--- examples/meson_routines.cc
+++ examples/meson_routines.cc
@@ -23,13 +23,45 @@ void FermToProp_s(LatticeStaggeredPropagator & Qprop, LatticeStaggeredFermion & 
 }
 
 
-void symm_shift(LatticeGaugeField &Umu, LatticeStaggeredFermion &q, int dir, int shift)
+void symm_shift_b(LatticeGaugeField &Umu, LatticeStaggeredFermion &q, int dir, int shift)
+{
+  GridBase *grid = Umu._grid;
+  LatticeColourMatrix U(grid);
+  U = peekLorentz(Umu, dir);
+
+  q =  0.5 * Cshift(q, dir, shift) +  Cshift(q, dir, -shift) ;
+
+}
+
+
+void symm_shift_a(LatticeGaugeField &Umu, LatticeStaggeredFermion &q, int dir, int shift)
 {
   GridBase *grid = Umu._grid;
   LatticeColourMatrix U(grid);
   U = peekLorentz(Umu, dir);
 
   q =  0.5 * U * Cshift(q, dir, shift) + adj(Cshift(U, dir, -shift))*Cshift(q, dir, -shift) ;
+
+}
+
+
+void symm_shift(LatticeGaugeField &Umu, LatticeStaggeredFermion &q, int dir, int shift)
+{
+  GridBase *grid = Umu._grid;
+  LatticeColourMatrix U(grid);
+  U = peekLorentz(Umu, dir);
+
+  q =  0.5 * U * Cshift(q, dir, shift) + 0.5 *adj(Cshift(U, dir, -shift))*Cshift(q, dir, -shift) ;
+
+}
+
+void symm_shift_c(LatticeGaugeField &Umu, LatticeStaggeredFermion &q, int dir, int shift)
+{
+  GridBase *grid = Umu._grid;
+  LatticeColourMatrix U(grid);
+  U = peekLorentz(Umu, dir);
+
+  q =  U * Cshift(q, dir, shift) ;
 
 }
 
@@ -145,12 +177,36 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
     LatticeCoordinate(coor[m], m);  
   }
 
-   r = coor[0] + coor[1] + coor[2] + coor[3];
-  //  r = coor[0] + coor[1] + coor[2] ;
+  Lattice<iScalar<vInteger> > tL(&Grid); LatticeCoordinate(tL,3);
+
+
+  const int XUP = 0 ;
+  const int YUP = 1 ;
+  const int ZUP = 2 ;
+  const int TUP = 3 ;
+
+
+  //      r =  coor[ZUP] + coor[TUP];
+  //     r =  coor[ZUP] ; 
+          r =  coor[TUP] ;   // no
+  //     r =  coor[XUP] + coor[YUP] + coor[3] ;  // no
+  //    r =  coor[XUP] + coor[YUP] ;  // no
+
+  //       r =  coor[XUP] + coor[3] ;   // no
+  //  r =  coor[XUP] + coor[YUP] +coor[ZUP] + coor[3] ;   //
+
+  //          r = coor[0] + coor[1] + coor[2] + coor[3];
+  //           r = coor[XUP] + coor[YUP] + coor[ZUP] ;
 
   //  phases = (-1)**(x+y+z+t)
   phases = where((mod(r,2)== (Integer) 0), one, minusOne);
-  
+
+
+#if 0      
+  int ntL = nt -1 ;
+  phases = where( tL ==(Integer)ntL, phases,-phases);
+#endif
+
   //    cout << phases << "\n" ;
 
 //////////////////////////////////////////////////////////////
@@ -163,7 +219,7 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
   LatticeStaggeredPropagator Qprop[2] = {&Grid, &Grid}  ;
 
   Qprop[1] = Qprop[0] = zero ;
-  const int x_dir = 0 ;  // x-direction
+  const int shift_dir = 2 ;  // x-direction  debug z-direction
 
   // Compute the staggered quark propagators
   for(int k=0; k<2; k++) {
@@ -184,7 +240,7 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
         pokeSite(cv,local_src,site);
       
         // shift the source
-        if(k) symm_shift(Umu, local_src, x_dir , 1); // do the unshifted qprop first
+        if(k) symm_shift(Umu, local_src, shift_dir , 1); // do the unshifted qprop first
 
         Ds.Mdag(local_src, out) ; // apply Mdagger
         local_src = out;
@@ -194,7 +250,7 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
         CG(HermOp,local_src,out);
 
         // apply the shifted operator to the sink
-	if(k) symm_shift(Umu, out, 0, 1); 
+	if(k) symm_shift(Umu, out, shift_dir, 1); 
 
         // add solution to propagator structure
         FermToProp_s(Qprop[k], out , ic  ) ; 
@@ -209,8 +265,9 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
   LatticeComplex  c[3] = {&Grid, &Grid, &Grid};
 
   for(int j=0; j<3; j++) {
-    c[j] = trace(adj(Qprop[0]) * Qprop[1]) ; 
-    c[j] = c[j] * phases;	
+       c[j] = trace(adj(Qprop[0]) * Qprop[1]) ; 
+    //    c[j] = trace(adj(Qprop[1]) * Qprop[1]) ; 
+        c[j] = c[j] * phases;	
   }
 
   //  this correlator over the lattice is summed over the spatial
@@ -226,12 +283,15 @@ void compute_onelink_rho(LatticeGaugeField & Umu, GridCartesian & Grid,
   int kmax = 1 ;
   string dir_name[4] = {"X", "Y", "Z", "T"}; 
 
-  for(int m=0; m<kmax; m++) {
+  //  for(int m=0; m<kmax; m++) {
+
+  { int m = shift_dir ;
     cout << "\nCorrelator in spin component " << dir_name[m]  << endl; 
     for(int tt = 0 ; tt < nt ; ++tt) {
       
         double ttt = real(corr[m][tt]) ;
-        cout << tt << " "  <<  ttt  << endl ;
+        double ttt_img = imag(corr[m][tt]) ;
+        cout << tt << " "  <<  ttt << "  "   << ttt_img  << endl ;
     }
   }
 
